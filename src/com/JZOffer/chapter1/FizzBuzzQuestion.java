7b8c6ac2f1c842d89763d4fd223102d6b8b3fa87
953761972 <953761972@qq.com>
2020-02-17 10:15:52 +0800

    Initial commit

---
+++ src/com/JZOffer/chapter1/FizzBuzzQuestion.java
@@ -0,0 +1,16 @@
+package com.JZOffer.chapter1;
+
+import java.util.List;
+
+public class FizzBuzzQuestion {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<String> fizzBuzz(int n) {
+        // write your code here
+
+    }
+
+}
